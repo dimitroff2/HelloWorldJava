70ef087d7b21b70a3715f5466d2aff69b78c7b92
dimitroff2 <dimitroff2@gmail.com>
2023-04-05 14:34:15 -0400

    Initial Commit Adding my name to practice GitHub

--- src/FirstClass.java
+++ src/FirstClass.java
@@ -1,6 +1,6 @@
 public class FirstClass {
     public static void main(String[] args) {
         System.out.print("Hello World");
-        System.out.print("Hello");
+        System.out.print(" Hello, Brandi");
     }
 }
