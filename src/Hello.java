8491984e4ce4d16cde58e75a01cd17dc0255b788
dimitroff2 <dimitroff2@gmail.com>
2023-04-05 15:07:29 -0400

    Practicing using if-then statements in Java.

--- src/Hello.java
+++ src/Hello.java
@@ -2,6 +2,16 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello, Brandi");
         //println adds a blank line after printing the text
-        System.out.print("Hello World");
+        boolean isAlien = false;
+        //  single = is assignment operator sets the value of the expression
+        if (isAlien == false) {
+            /*
+             == equality  operator test to see if two operands are considered equal and returns boolean value
+            if statement does not have a semicolon because the statement is spanning two lines
+            when semicolon is placed after the if statement it makes that print statement no longer depend on it so it will print code regardless
+            */
+            System.out.println("It is not an alien!");
+            System.out.println("And I am scared of aliens");
+        }
     }
 }
