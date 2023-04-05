84e043eec06827d9fc914ae26e5ff3fbe3ced95c
dimitroff2 <dimitroff2@gmail.com>
2023-04-05 14:42:41 -0400

    Adding additional classes for practice and using println instead of print to make a blank line in between statements.

---
+++ src/Hello.java
@@ -0,0 +1,7 @@
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, Brandi");
+        //println adds a blank line after printing the text
+        System.out.print("Hello World");
+    }
+}
